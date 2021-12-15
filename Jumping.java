925746283db3d664ff1178191e3f195960d62411
19pa1a0413 <60500469+19pa1a0413@users.noreply.github.com>
2021-12-15 16:32:35 +0530

    Create Jumping.java

---
+++ Jumping.java
@@ -0,0 +1,16 @@
+public class  Jumping{
+	private int startingpoint;
+	private int endingpoint;
+	public Jumping(int startingpoint,int endingpoint){
+	this.startingpoint=startingpoint;
+	this.endingpoint=endingpoint;
+	}
+	public int getStartingPoint(){
+		return startingpoint;
+	}
+	public int getEndingPoint(){
+		return endingpoint;
+
+		
+	}
+}
